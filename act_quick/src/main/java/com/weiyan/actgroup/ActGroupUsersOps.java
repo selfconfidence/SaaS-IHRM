4140c7e36ed32615f6cc36f970d6a6ce08f7f018
selfconfidence <mynameisweiyan@gmail.com>
2019-07-24 20:00:56 +0800

    actitivi的组概念 -author weiyan

---
+++ act_quick/src/main/java/com/weiyan/actgroup/ActGroupUsersOps.java
@@ -0,0 +1,14 @@
+package com.weiyan.actgroup;
+
+/**
+ * @author mister_wei
+ * @version 1.1.1
+ * @title Gropu在Activiti中的引用规则
+ * @package com.weiyan
+ * @date 2019/7/24 10:36
+ *
+ * 业务场景: 请假单--> 提交请假单,部门经理如果只有一个那么就无法请假,所有就有了userGroup的概念,多用户处理任务
+ * 多人处理任务
+ */
+public class ActGroupUsersOps {
+}
